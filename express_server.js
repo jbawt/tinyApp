55961dbe689ae4039497fd88d889c805c1f738e0
Josh Bawtinheimer <jbawtinheimer@gmail.com>
2020-08-31 23:05:37 +0000

    added urls_show.ejs file and new route

--- express_server.js
+++ express_server.js
@@ -20,6 +20,11 @@ app.get("/urls.json", (req, res) => {
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
 })
 
 app.get("/hello", (req, res) => {
