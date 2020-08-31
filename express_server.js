5114561e973509ae4f802907d11e6ab8daf715cc
Josh Bawtinheimer <jbawtinheimer@gmail.com>
2020-08-31 19:52:27 +0000

    adding json urlDatabase response

--- express_server.js
+++ express_server.js
@@ -12,6 +12,10 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
