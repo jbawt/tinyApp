50c34a8fca16b1f6fa7df626e09a93559ffe8e2d
Josh Bawtinheimer <jbawtinheimer@gmail.com>
2020-09-01 02:22:12 +0000

    updated comments for code

--- express_server.js
+++ express_server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = 8080;
 
+// random alphanumeric string
 const generateRandomString = () => {
   return Math.random().toString(36).slice(2, 8);
 };
@@ -10,25 +11,30 @@ const generateRandomString = () => {
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+// url storage
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+// shows all stored shortened urls corresponding to their long url pairs
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+// renders page for creating new shortened url
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+// renders results for shortened url
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 
+// stores new short url with random key
 app.post("/urls", (req, res) => {
   let key = generateRandomString();
   let templateVars = { shortURL: key, longURL: req.body.longURL };
@@ -36,6 +42,7 @@ app.post("/urls", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+// redirect to main url
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
